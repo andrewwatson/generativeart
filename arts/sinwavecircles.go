77a5ed186f160ba7be8d0188fc130e067059f74b
Andrew Watson <andy.watson@driveclutch.com>
2021-10-30 02:47:28 -0400

    Added new Arts module: Sin Wav Circles

---
+++ arts/sinwavecircles.go
@@ -0,0 +1,91 @@
+package arts
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+
+	"github.com/andrewwatson/generativeart"
+	"github.com/andrewwatson/generativeart/common"
+	"github.com/fogleman/gg"
+)
+
+type sinwavecircles struct {
+	amplitude float64
+	spacing   float64
+	noise     *common.PerlinNoise
+	depth     int
+}
+
+// NewBlackHole returns a blackhole object.
+func NewSinWaveCircles(amplitude int, spacing int, depth int) *sinwavecircles {
+	return &sinwavecircles{
+		amplitude: float64(amplitude),
+		spacing:   float64(spacing),
+		noise:     common.NewPerlinNoise(),
+		depth:     depth,
+	}
+}
+
+func (swc *sinwavecircles) Generative(c *generativeart.Canva) {
+
+	ctex := gg.NewContextForRGBA(c.Img())
+	swc.recursionDraw(ctex, c, float64(c.Width()), swc.depth)
+}
+
+func (swc *sinwavecircles) recursionDraw(ctex *gg.Context, c *generativeart.Canva, x float64, depth int) {
+	if depth <= 0 {
+		return
+	}
+	swc.draw(ctex, c, x)
+	swc.recursionDraw(ctex, c, 1*x/4.0, depth-1)
+	swc.recursionDraw(ctex, c, 2*x/4.0, depth-1)
+	swc.recursionDraw(ctex, c, 3*x/4.0, depth-1)
+}
+
+// Generative draws a black hole image.
+func (swc *sinwavecircles) draw(ctex *gg.Context, c *generativeart.Canva, x float64) {
+
+	// noise := common.NewPerlinNoise()
+	// kMax := common.RandomRangeFloat64(0.5, 1)
+	ctex.SetLineWidth(0.4)
+	ctex.SetColor(common.White)
+
+	startingX := float64(c.Width()) * 0.05
+	endingX := float64(c.Width()) * 0.95
+
+	midpointY := float64(c.Height() / 2)
+
+	noise := swc.noise.Noise3D(x*0.02+123.234, (1-x)*0.02, 345.4123)
+	noise = math.Pow(noise, 0.5)
+	a2 := common.Remap(noise, 0.15, 0.85, 1.0, 16.0)
+
+	for i := startingX; i < endingX; i += swc.spacing {
+
+		var lw float64
+		if rand.Float64() < 0.8 {
+			lw = 1.2
+		} else {
+			lw = common.RandomRangeFloat64(1.0, common.RandomRangeFloat64(1, 3))
+		}
+		ctex.SetLineWidth(lw)
+
+		radianX := gg.Radians(i * 0.25)
+		SinX := math.Sin(radianX) * swc.amplitude
+		r := math.Pow(rand.Float64(), 2) * 100
+
+		fmt.Printf("X: %0.4f Rx: %0.4f Sin: %0.4f radius: %0.4f\n", i, radianX, SinX, r)
+
+		cls := c.Opts().ColorSchema()[rand.Intn(len(c.Opts().ColorSchema()))]
+		ctex.SetColor(cls)
+
+		// inflationFactor := rand.Float64() * 25
+		// yValue :=
+		ctex.DrawCircle(i, midpointY+SinX*a2, r)
+		ctex.Stroke()
+
+	}
+
+	ctex.ClearPath()
+
+}
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/aquilax/go-perlin v1.0.0
 	github.com/fogleman/gg v1.3.0
 	github.com/llgcode/draw2d v0.0.0-20200930101115-bfaf5d914d1e
+	github.com/satori/go.uuid v1.2.0
 	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
 )
